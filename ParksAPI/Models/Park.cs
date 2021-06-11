764a25e2a23990152bf3f01755d58dba89dbb5b8
cass1618 <cass16180@gmail.com>
2021-06-11 11:58:13 -0700

    :national_park: Create Models

---
+++ ParksAPI/Models/Park.cs
@@ -0,0 +1,10 @@
+namespace ParksAPI.Models
+{
+  public class Park 
+  {
+    public int ParkId { get; set; }
+    public string ParkName { get; set; }
+    public string Size { get; set; }
+    public string Description { get; set; }
+  }
+}
