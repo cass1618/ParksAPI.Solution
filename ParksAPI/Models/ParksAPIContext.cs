062513936b404748db2bf8d46590dd3e72f726cb
cass1618 <cass16180@gmail.com>
2021-06-14 00:59:39 -0700

    :memo: Implement Swagger

--- ParksAPI/Models/ParksAPIContext.cs
+++ ParksAPI/Models/ParksAPIContext.cs
@@ -18,7 +18,7 @@ namespace ParksAPI.Models
                 new Park { ParkId = 1, ParkName = "Drake Park", Size = "Medium", Description="Music, river, events" },
                 new Park { ParkId = 2, ParkName = "Esther Short", Size = "Medium", Description="Gazebos, playground" },
                 new Park { ParkId = 3, ParkName = "Mount Tabor", Size = "Large", Description="Music, Dancing, Trees" },
-                new Park { ParkId = 4, ParkName = "Cottonwood", Size = "Small", Description="Beach, pavillions, grills" }
+                new Park { ParkId = 4, ParkName = "Cottonwood", Size = "Small", Description="Beach, pavilions, grills" }
             );
         }
     }
--- ParksAPI/Startup.cs
+++ ParksAPI/Startup.cs
@@ -1,50 +1,62 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using ParksAPI.Models;
 
 namespace ParksAPI
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<ParksAPIContext>(opt =>
-                opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
-            services.AddControllers();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                // app.UseSwagger();
-                // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ParksAPI v1"));
-            }
-
-            // app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        //   .AddEntityFrameworkStores<ParksAPIContext>()
+      services.AddDbContext<ParksAPIContext>(opt =>
+                opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+      services.AddControllers();
+      services.AddSwaggerGen(swagger =>
+     {
+       //This is to generate the Default UI of Swagger Documentation    
+       swagger.SwaggerDoc("v1", new OpenApiInfo
+       {
+         Version = "v1",
+         Title = "ASP.NET 5 Web API",
+         Description = "Authentication and Authorization in ASP.NET 5 with JWT and Swagger"
+       });
+     });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+                {
+                  c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                });
+    //   app.UseHttpsRedirection();
+      app.UseRouting();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
